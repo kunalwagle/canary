f62c3c3cc40edaa425104f4615660059a31973d6
Kunal <kunal@roi.com>
2017-08-29 06:20:49 -0700

    Adding serializable interface for Order class

--- forex-trader/src/main/java/com/creditsuisse/london/forex_trader/orders/Order.java
+++ forex-trader/src/main/java/com/creditsuisse/london/forex_trader/orders/Order.java
@@ -1,7 +1,11 @@
 package com.creditsuisse.london.forex_trader.orders;
 
-public class Order {
-	
+import java.io.Serializable;
+
+public class Order implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
 	private long id;
 	private int quantity;
 	private float price;
